ddc791497b094c2ca85bb3e3023fa07aed81eaa0
guifaxina <99680960+guifaxina@users.noreply.github.com>
2022-09-02 10:09:43 -0300

    style: create basic structure

---
+++ app/src/App.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components' 
+import { Header } from './components/Header/Index'
+import { GlobalStyle } from './styles/global'
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App
